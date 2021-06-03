dfe2357f6eee8b535b048e9b21883cb80a49cb1c
152120161084_ÖmerŞentürk <152120161084@ogrenci.ogu.edu.tr>
2021-06-03 22:19:15 +0300

    Added customer and music_cd classes

--- OopII-Project/OopII-Project/Classes/customer.cs
+++ OopII-Project/OopII-Project/Classes/customer.cs
@@ -1,10 +1,126 @@
-﻿using System;
+﻿/**
+ * @File customer.cs
+ * @Date June, 2021
+ * @Author Omer SENTURK (omersenturk95@gmail.com)
+ *
+ */
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 namespace OopII_Project
 {
-    class customer
+
+    /**
+    * \brief Class for customers who is using this app
+    *
+    */
+    class Customer
     {
+        private int customer_ID;
+        private string name;
+        private string address;
+        private string email;
+        private string username;
+        private string pass;
+
+        /**
+        * \brief Getter and Setter methods for Customer class attributes 
+        * \param all attributes
+        * \return all attributes
+        */
+        public int CustomerID
+        {
+            get
+            {
+                return customer_ID;
+            }
+
+            set
+            {
+                customer_ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return pass;
+            }
+
+            set
+            {
+                pass = value;
+            }
+        }
+
+        public void print_customer_details()
+        {
+            
+        }
+        public void save_customer()
+        {
+            
+        }
+        public void print_customer_purchases()
+        {
+            
+        }
+
     }
 }
--- OopII-Project/OopII-Project/Classes/music_cd.cs
+++ OopII-Project/OopII-Project/Classes/music_cd.cs
@@ -1,10 +1,83 @@
-﻿using System;
+﻿/**
+ * @File music_cd.cs
+ * @Date June, 2021
+ * @Author Omer SENTURK (omersenturk95@gmail.com)
+ *
+ */
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OopII_Project
 {
-    class music_cd
+    /**
+    * \brief Class for MusicCD types product inherited by Product
+    *
+    */
+    class MusicCD : Product
     {
+
+        private string _singer; /*!< Def of singer */
+        private string _type; /*!< Def of type */
+
+        /**
+        * \brief Constructor function of the MusicCD class
+        * \param ID, name, price, singer, type
+        *  this function assigns to parameters to new instance 
+        */
+        public MusicCD(int ID = 0, string name = "noname", double price = 0, string singer = "N/A", string type = "N/A") : base(ID, name, price)
+        {
+            _singer = singer;
+            _type = type;
+        }
+
+        /**
+        * \brief Getter and Setter medhod for singer 
+        * \param singer
+        * \return singer
+        */
+        public string Singer
+        {
+            get
+            {
+                return _singer;
+            }
+
+            set
+            {
+                _singer = value;
+            }
+        }
+
+        /**
+        * \brief Getter and Setter medhod for type 
+        * \param type
+        * \return type
+        */
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                _type = value;
+            }
+        }
+
+        /**
+        * \brief overrided function of printProperties
+        * \return string type values
+        */
+        public override string printProperties()
+        {
+            string[] basePrint = base.printProperties().Split(';');
+            return (basePrint[0] + Environment.NewLine + "---------------" + Environment.NewLine + _singer + Environment.NewLine + Environment.NewLine + _type + Environment.NewLine + Environment.NewLine + basePrint[1].ToString() + " TL");
+        }
     }
 }
