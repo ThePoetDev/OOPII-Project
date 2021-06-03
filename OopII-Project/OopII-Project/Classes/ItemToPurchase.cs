0eb0f9e859af3ab78ad4f1196c0ed93a623019e4
Servet Kıraç <152120131105@ogrenci.ogu.edu.tr>
2021-06-03 22:59:36 +0300

    itemsToPurchase

--- OopII-Project/OopII-Project/Classes/ItemToPurchase.cs
+++ OopII-Project/OopII-Project/Classes/ItemToPurchase.cs
@@ -6,5 +6,27 @@ namespace OopII_Project.Classes
 {
     class ItemToPurchase
     {
+        private Product product;
+        private int quantity;
+        
+        public ItemToPurchase(Product _product,int _qunatity = 0)
+        {
+            product = _product;
+            quantity = _qunatity;
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set{if (value >0){quantity = value;}}
+        }
+        public Product Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+
+
     }
 }
