59b5e1126a64377766877e413d40925368bdba28
ThePoetDev <152120161035@ogrenci.ogu.edu.tr>
2021-06-03 22:01:18 +0300

    Added product class

--- OopII-Project/OopII-Project/Classes/Product.cs
+++ OopII-Project/OopII-Project/Classes/Product.cs
@@ -1,10 +1,104 @@
-﻿using System;
+﻿/**
+ * @File Product.cs
+ * @Date June, 2021
+ * @Author Bülent Buğrahan DALLI (bugrahandalli@gmail.com)
+ *
+ *
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace OopII_Project
-{
-    class Product
-    {
+namespace OopII_Project {
+    /**
+    * \brief Abstract Class of Product
+    *
+    */
+    public abstract class Product {
+        private int _id;  /*!< Def ID */
+        private string _name;  /*!< Def Name */
+        private double _price;  /*!< Def Price */
+        private string _image;
+
+
+        /**
+        * \brief Constructor of Product
+        * \param id, name, price
+        *  this method starts the process of assigning new data.
+        */
+        public Product(int id = 0, string name = "noname", double price = 0) {
+            _id = id;
+            _name = name;
+            _price = price;
+        }
+
+        /**
+        * \brief Getter and setter methods of ID
+        * \param ID
+        * \return ID
+        */
+        public int ID {
+            get {
+                return _id;
+            }
+
+            set {
+                _id = value;
+            }
+        }
+
+        /**
+        * \brief Getter and setter methods of Name
+        * \param name
+        * \return name
+        */
+        public string Name {
+            get {
+                return _name;
+            }
+
+            set {
+                _name = value;
+            }
+        }
+
+        /**
+        * \brief Getter and setter methods of Price
+        * \param price
+        * \return price
+        */
+        public double Price {
+            get {
+                return _price;
+            }
+
+            set {
+                _price = value;
+            }
+        }
+
+        /**
+        * \brief Getter and setter methods of Image
+        * \param image
+        * \return image
+        */
+        public string Image {
+            get {
+                return _image;
+            }
+
+            set {
+                _image = value;
+            }
+        }
+
+        /**
+        * \brief virtual method of printProperties
+        * method will be overridden by inherited classes
+        * \return string value
+        */
+        public virtual string printProperties() {
+            return ("Id: " + _id + "- Name : " + _name + " - Price: " + _price.ToString("0.00"));
+        }
     }
 }
