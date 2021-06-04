c1e2a895655edca417d63203d7bf962e6141c9eb
ThePoetDev <152120161035@ogrenci.ogu.edu.tr>
2021-06-04 19:32:13 +0300

    Made some diff on Orderscreen form

--- OopII-Project/OopII-Project/OrderScreen/OrderScreen.cs
+++ OopII-Project/OopII-Project/OrderScreen/OrderScreen.cs
@@ -8,20 +8,17 @@ using System.Windows.Forms;
 
 using System.Data.SqlClient;
 
-namespace OopII_Project
-{
-    public partial class OrderScreen : Form
-    {
-        string current, item1, item2, price, isbn, author, publisher, page;
+namespace OopII_Project {
+    public partial class OrderScreen : Form {
+        string current, item1, item2, price, isbn, author, publisher, page, amount;
         SqlDataReader dr;
-        SqlConnection con;
-        SqlDataAdapter da;
-        DataTable dt;
+        SqlConnection con, con2;
+        SqlDataAdapter da,da2;
+        DataTable dt,dt2;
         DataSet ds;
         SqlCommand command;
 
-        public OrderScreen(string _current)
-        {
+        public OrderScreen(string _current) {
             InitializeComponent();
             current = _current;
         }
@@ -39,18 +36,19 @@ namespace OopII_Project
             con = new SqlConnection("Data Source=SQL5050.site4now.net;Initial Catalog=db_a756f7_oopii;User Id=db_a756f7_oopii_admin;Password=Oop2project");
             da = new SqlDataAdapter("SELECT * FROM ShoppingCarts Where username='" + current + "'", con);
             dt = new DataTable();
+            dt2 = new DataTable();
             con.Open();
             da.Fill(dt);
-            con.Close();
             item1 = dt.Rows[0]["item1"].ToString();
             item2 = dt.Rows[0]["item2"].ToString();
             orderNameLabel1.Text = item1;
             orderNameLabel2.Text = item2;
-           
-            con.Open();
+
             dt.Clear();
             da = new SqlDataAdapter("SELECT * FROM Products Where name='" + item1 + "'", con);
+            da2 = new SqlDataAdapter("SELECT * FROM ShoppingCarts Where username='" + current + "'", con);
             da.Fill(dt);
+            da2.Fill(dt2);
             con.Close();
 
             try {
@@ -59,11 +57,14 @@ namespace OopII_Project
                 author = dt.Rows[0]["author"].ToString();
                 publisher = dt.Rows[0]["publisher"].ToString();
                 page = dt.Rows[0]["page"].ToString();
+                amount = dt2.Rows[0]["item1amount"].ToString();
+
 
                 contentLabel1.Text = "ISBN: " + isbn + Environment.NewLine + "Author: " + author + Environment.NewLine + "Publisher: " + publisher + Environment.NewLine + "Page: " + page;
-                costLabel1.Text = "Cost: " + price;
+                costLabel1.Text = "Cost: " + price + "x" + amount + "=" + (Int32.Parse(price) * Int32.Parse(amount));
                 deliveryInformationLabel1.Text = "Delivered";
-            }catch(Exception ex) {
+            }
+            catch (Exception ex) {
                 MessageBox.Show(ex.ToString());
             }
 
@@ -79,9 +80,10 @@ namespace OopII_Project
                 author = dt.Rows[0]["author"].ToString();
                 publisher = dt.Rows[0]["publisher"].ToString();
                 page = dt.Rows[0]["page"].ToString();
+                amount = dt2.Rows[0]["item2amount"].ToString();
 
                 contentLabel2.Text = "ISBN: " + isbn + Environment.NewLine + "Author: " + author + Environment.NewLine + "Publisher: " + publisher + Environment.NewLine + "Page: " + page;
-                costLabel2.Text = "Cost: " + price;
+                costLabel2.Text = "Cost: " + price + "x" + amount + "=" + (Int32.Parse(price) * Int32.Parse(amount));
                 deliveryInformationLabel2.Text = "Delivered";
             }
             catch (Exception ex) {
