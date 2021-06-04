c6f8407ad9d4ba08e67450038ba2791b3caf13a4
ThePoetDev <152120161035@ogrenci.ogu.edu.tr>
2021-06-04 03:23:11 +0300

    Added signup and login system

--- OopII-Project/OopII-Project/SignUpScreen/SignUpScreen.cs
+++ OopII-Project/OopII-Project/SignUpScreen/SignUpScreen.cs
@@ -6,43 +6,75 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
-namespace OopII_Project
-{
-    public partial class SignUp : Form
-    {
-        public SignUp()
-        {
+using System.Data.SqlClient;
+
+namespace OopII_Project {
+    public partial class SignUp : Form {
+        public SignUp() {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        private void label1_Click(object sender, EventArgs e) {
 
         }
 
-        private void SignUpScreen_Load(object sender, EventArgs e)
-        {
+        private void SignUpScreen_Load(object sender, EventArgs e) {
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
+        private void textBox2_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+        private void textBox1_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void lblUsername_Click(object sender, EventArgs e)
-        {
+        private void lblUsername_Click(object sender, EventArgs e) {
 
         }
 
-        private void btnSignUp_Click(object sender, EventArgs e)
-        {
+        private void btnSignUp_Click(object sender, EventArgs e) {
+            Customer newc = new Customer();
+            string message = "";
+            if (tbName.Text.Length < 1 || tbSurname.Text.Length < 1 || tbUsername.Text.Length < 1 || tbPassword.Text.Length < 1) {
+                MessageBox.Show("Mail ve Adres harici bilgiler boş bırakılamaz.");
+            }
+            else {
+                newc.Name = tbName.Text.ToString();
+                newc.Email = tbEmail.Text.ToString();
+                newc.Username = tbUsername.Text.ToString();
+                newc.Password = tbPassword.Text.ToString();
+                newc.Address = textBox1.Text.ToString();
 
+                SqlConnection con = new SqlConnection("Data Source=SQL5050.site4now.net;Initial Catalog=db_a756f7_oopii;User Id=db_a756f7_oopii_admin;Password=Oop2project");
+                SqlConnection con2 = new SqlConnection("Data Source=SQL5050.site4now.net;Initial Catalog=db_a756f7_oopii;User Id=db_a756f7_oopii_admin;Password=Oop2project");
+             
+                if (con.State == ConnectionState.Closed) {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Select * From Customer Where username='" + newc.Username + "'", con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read()) {
+                        MessageBox.Show("Aynı isimli kullanıcı bulunmakta lütfen farklı bir kullanıcı adı kullanınız.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else {
+                        con2.Open();
+                        string command = "insert into Customer(Name,Adress,Email,username,password) values (@name,@adress,@email,@username,@password)";
+                        SqlCommand com = new SqlCommand(command, con2);
+                        com.Parameters.AddWithValue("@name", newc.Name);
+                        com.Parameters.AddWithValue("@adress", newc.Address);
+                        com.Parameters.AddWithValue("@email", newc.Email);
+                        com.Parameters.AddWithValue("@username", newc.Username);
+                        com.Parameters.AddWithValue("@password", newc.Password);
+                        com.ExecuteNonQuery();
+
+                        MessageBox.Show("Kaydınız başarı ile oluşturuldu.", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        con2.Close();
+
+                    }
+                }
+               
+            }
         }
     }
 }
--- OopII-Project/OopII-Project/login/LoginScreen.cs
+++ OopII-Project/OopII-Project/login/LoginScreen.cs
@@ -6,18 +6,60 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace OopII_Project {
     public partial class LoginScreen : Form {
+
+
+        SqlDataReader dr;
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataTable dt;
+        DataSet ds;
+        SqlCommand command;
+
         public LoginScreen() {
             InitializeComponent();
         }
 
 
         private void loginButton_Click(object sender, EventArgs e) {
-            Form homepageScreen = new HomePageScreen();
-            this.Hide();
-            homepageScreen.ShowDialog();
-            this.Close();
+            if (usernameTextBox.Text != null || usernameTextBox.Text != null) {
+                con = new SqlConnection("Data Source=SQL5050.site4now.net;Initial Catalog=db_a756f7_oopii;User Id=db_a756f7_oopii_admin;Password=Oop2project");
+                da = new SqlDataAdapter("Select * From Customer Where username='" + usernameTextBox.Text.Trim()
+                    + "' and password='" + passwordTextBox.Text.Trim() + "'", con);
+                dt = new DataTable();
+            }
+            try {
+                con.Open();
+                da.Fill(dt);
+                con.Close();
+
+                if (dt.Rows.Count == 0) {
+                    MessageBox.Show("Kayıtlı kullanıcı bulunamadı!", "Hata",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                else {
+                    Form homepageScreen = new HomePageScreen();
+                    this.Hide();
+                    homepageScreen.ShowDialog();
+                    this.Close();
+                }
+                
+
+            }
+            catch (Exception a) {
+                MessageBox.Show(a.ToString());
+            }
+         
+
+
+
+
+
+            
         }
 
         private void signupButton_Click(object sender, EventArgs e) {
